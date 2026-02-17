158cb480bb82a38a188e40d9397311d129859967
egarciao-max <egarciao@irsl.edu.mx>
2026-02-17 03:59:17 -0800

    Crear config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+    SUPABASE_URL: "https://njulviuanxgyvrkwwwfi.supabase.co",
+    // Uses the Anon key (Public)
+    SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qdWx2aXVhbnhneXZya3d3d2ZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzMDEwNDgsImV4cCI6MjA4Njg3NzA0OH0.Y7g2RNsnM68Phv6yB7ZrXjJljW88LTUzuiqveDzW6G4",
+    OPENAI_KEY: "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"
+};
